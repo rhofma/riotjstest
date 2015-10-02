61aac1f4fefc26108af56c804e9c5156f079104d
Roman Hofmann <rmnhofma@gmail.com>
2015-10-02 15:06:49 +0200

    delete .tags dir

--- .tags/all.js
+++
@@ -1,6 +0,0 @@
-riot.tag('hello-form', '<form> <input type="text" name="greet"> <button>say yow</button> </form>', function(opts) {
-
-});
-riot.tag('hello-world', '<h3>Hello {opts.greet}</h3>', function(opts) {
-
-});
